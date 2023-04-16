223ed3cacd5e919eead286a04d753b7d813c3878
Gabriel-pagnan <gabrielviniciuspagnan@gmail.com>
2023-04-16 16:33:41 -0300

    fix: cpf ja cadastrado

--- src/paciente/paciente.service.ts
+++ src/paciente/paciente.service.ts
@@ -4,14 +4,14 @@ import { NotFoundException } from "@nestjs/common";
 import { Injectable } from "@nestjs/common/decorators";
 import { CreatePacienteDTO } from "./dto/paciente.create.dto";
 import { UpdatePacienteDTO } from "./dto/paciente.update.dto";
-import {Password} from '../utils/random.password'
+import { Password } from '../utils/random.password'
 import { CPF } from "../utils/validate.cpf";
 
 @Injectable()
 export class PacienteService {
-    constructor(@InjectModel('CreatePacienteDTO') private readonly pacienteModel: Model<CreatePacienteDTO>) {}
+    constructor(@InjectModel('CreatePacienteDTO') private readonly pacienteModel: Model<CreatePacienteDTO>) { }
 
-    async getAll(){
+    async getAll() {
         return await this.pacienteModel.find().exec()
     }
 
@@ -24,37 +24,37 @@ export class PacienteService {
     async create(data: CreatePacienteDTO) {
         const password = new Password()
         const cpfvalid = new CPF()
-        data.password = password.gerar()
-        
-        if(!cpfvalid.validation(data.cpf)){
-            throw new NotFoundException('CPF inválido.');
-        }
-        if(this.cpfExists(data.cpf)){
-            throw new NotFoundException('CPF já cadastrado.');
-        }
+        try {
+            data.password = password.gerar()
 
-        const createdNew = new this.pacienteModel(data);
-        return await createdNew.save()
+            if (!cpfvalid.validation(data.cpf)) {
+                throw new NotFoundException('CPF inválido.');
+            }
+
+            const createdNew = new this.pacienteModel(data);
+            return await createdNew.save()
+        } catch (error) {
+            if(error.code === 11000){
+                throw new NotFoundException('CPF já cadastrado.')
+            }
+            throw new NotFoundException(error)
+        }
     }
 
     async update(id: string, data: UpdatePacienteDTO) {
         this.exists(id)
-        await this.pacienteModel.updateOne({_id: id}, data).exec();
+        await this.pacienteModel.updateOne({ _id: id }, data).exec();
 
         return this.getById(id)
     }
 
     async delete(id: string) {
-        this.exists(id)
-        return this.pacienteModel.deleteOne({_id: id}).exec()
+        return this.pacienteModel.deleteOne({ _id: id }).exec()
     }
 
     async exists(id: string) {
-        if(!(await this.pacienteModel.findById({_id: id}))){
+        if (!(await this.pacienteModel.findById({ _id: id }))) {
             throw new NotFoundException('O usuário não existe.');
         }
-    }
-    async cpfExists(cpf: string) {
-        return await this.pacienteModel.findOne({cpf: cpf})
     }
 }
